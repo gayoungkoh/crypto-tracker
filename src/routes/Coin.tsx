adbe01a372fc46f13af81471b2116a2273a1d31e
gayoung.koh <gayoung.koh@qcells.com>
2025-03-13 23:07:39 +0900

    coin data, data types

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -4,14 +4,36 @@ import {
   HEADER_CLASSNAME,
   TITLE_CLASS_NAME,
 } from "@/constants/class-name";
-import { CoinRouteParams } from "@/types/coin";
-import { useState } from "react";
+import { CoinRouteParams, InfoData, PriceData } from "@/types/coin";
+import { fetchData } from "@/utils/api-helper";
+import { useEffect, useState } from "react";
 import { useLocation, useParams } from "react-router";
 
 const Coin = () => {
   const { coinId } = useParams<CoinRouteParams>();
   const [loading, setLoading] = useState(true);
   const { state } = useLocation();
+  const [info, setInfo] = useState<InfoData>();
+  const [priceInfo, setPriceInfo] = useState<PriceData>();
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const infoData = await fetchData<InfoData>(
+          `https://api.coinpaprika.com/v1/coins/${coinId}`
+        );
+        const priceData = await fetchData<PriceData>(
+          `https://api.coinpaprika.com/v1/tickers/${coinId}`
+        );
+        setInfo(infoData);
+        setPriceInfo(priceData);
+
+        setLoading(false);
+      } catch (error) {
+        console.error(error);
+      }
+    })();
+  }, []);
 
   return (
     <div className={CONTAINER_CLASS_NAME}>
