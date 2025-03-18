c4fb0c74cdefaf0c32190443597742f53f3b95d1
gayoung.koh <gayoung.koh@qcells.com>
2025-03-18 23:55:26 +0900

    add sort to rank column

--- src/components/coin-list-config.tsx
+++ src/components/coin-list-config.tsx
@@ -28,7 +28,17 @@ export const coinListColumns: ColumnDef<Coin>[] = [
   },
   {
     accessorKey: "rank",
-    header: "Rank",
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          Rank
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      );
+    },
   },
   {
     accessorKey: "is_new",
